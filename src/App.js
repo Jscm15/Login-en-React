eb47e6707f32d4c3c2b108d8bfcfca32e59d7e97
Jscm15 <jscmsebastian@gmail.com>
2022-11-29 20:06:48 -0500

    CRA-Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const adminUser ={
+    
+    email: "admin@admin.com",
+    password: "admin123",
 }
 
+  const [user, setUser] =  useState({email:"", password:""});
+  const [error, setError] = useState("");
+  
+  const Login = details => {
+    console.log(details);
+    if (details.email === adminUser.email && details.password === adminUser.password) {
+      console.log("Logged in");
+    } else {
+      console.log("Caracteres no Validos");
+      setError("Por favor vuelva a intentarlo, sus credenciales son inválidas")
+    }
+  }
+  
+const Logout = () =>{
+  setUser({email:"", password:""});
+}
+
+return(
+<div className="App">
+{(user.email !== "") ? (
+<div className="welcome">
+<h2>Welcome, <span>{user.email}</span></h2>
+<button onClick={Logout}>Logout</button>
+</div>
+
+  ) : (
+    <Login Login={Login} error={error}/>
+  )
+  }
+</div>
+)}
 export default App;
